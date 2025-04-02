e0c4bf4753f5daa1c897a70101e2a603da4dd3ce
Vigintillionn <yarne.ramakers@gmail.com>
2025-04-03 00:34:08 +0200

    Implemented Fibonacci function and improved error handling in main

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
+use interpreter::Eval;
 pub use lexer::{tokens::Token, tokenize};
 pub use errors::{LexerError, LexerResult};
 
 mod lexer;
 mod parser;
+mod interpreter;
 mod errors;
 
 fn main() -> Result<(), LexerError> {
   let code = "
-  proc main(a: int, b: int) -> int {
-    var a = 10;
-    ret;
-  }
+    proc fib(n: int) -> int {
+      if (n <= 1) {
+        ret n;
+      }
+
+      ret fib(n - 1) + fib(n - 2);
+    }
+
+    print(fib(10));
   ";
 
   let tokens = tokenize(code)?;
   println!("{:?}", tokens);
 
-  let ast = parser::parse(tokens.as_slice());
+  let ast = parser::parse(tokens.as_slice()).map_err(|e| {
+    eprintln!("{}", e);
+    LexerError::UnexpectedEOF
+  })?;
   println!("{:?}", ast);
+
+  ast.eval(&mut interpreter::Environment::new());
 
   Ok(())
 }

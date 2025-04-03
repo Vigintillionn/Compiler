f48d19d7d158bedc46b453928f14ef7ddba1a92b
Vigintillionn <yarne.ramakers@gmail.com>
2025-04-03 23:21:00 +0200

    Removed unnecessary code

--- src/errors.rs
+++ src/errors.rs
@@ -9,30 +9,6 @@ pub enum LexerError {
   Other(String),
 }
 
-impl std::fmt::Display for LexerError {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    match self {
-      LexerError::InvalidCharacter(c, line, col, src) => {
-        write!(f, "Invalid character '{}' at line {}, column {}: {}", c, line, col, src)
-      }
-      LexerError::UnterminatedString(line, col, src) => {
-        write!(f, "Unterminated string at line {}, column {}: {}", line, col, src)
-      }
-      LexerError::InvalidNumber(num, line, col, src) => {
-        write!(f, "Invalid number '{}' at line {}, column {}: {}", num, line, col, src)
-      }
-      LexerError::UnexpectedEOF(line, col) => {
-        write!(f, "Unexpected EOF at line {}, column {}", line, col)
-      }
-      LexerError::Other(msg) => {
-        write!(f, "{}", msg)
-      }
-    }
-  }
-}
-
-impl std::error::Error for LexerError {}
-
 pub type ParseResult<T> = Result<T, ParseError>;
 
 #[derive(Debug, Clone, PartialEq)]

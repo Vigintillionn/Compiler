0034f0846348cbb0fa8c07cd02341930a7a15ca2
Vigintillionn <yarne.ramakers@gmail.com>
2025-04-14 02:32:43 +0200

    Integrate static analysis for type checking before program evaluation in main function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use errors::report_error;
-use interpreter::Eval;
+use interpreter::eval_program;
 pub use lexer::{tokenize, tokens::Token};
 pub use parser::parse;
+use staticanalysis::type_check_program;
 
+mod environment;
 mod errors;
 mod interpreter;
 mod lexer;
 mod parser;
 mod sourcelines;
+mod staticanalysis;
 
 fn main() -> Result<(), String> {
     // let code: &str = "proc fib(n: int) -> int {
@@ -25,10 +28,38 @@ fn main() -> Result<(), String> {
 
     //   print(fib(10));
     // ";
-    let code = "proc a(b: int, c: int) -> int {
-        ret b * c;
-    }
-    print(10 + a(20 + 10 * 2, 3));";
+    // let code = "
+    // proc fib(n: int) -> int {
+    //     var a: int = 0;
+    //     for (var i = 0; i < n; i = i + 1) {
+    //         if (i == 4) {
+    //             break;
+    //         }
+    //         a = a + i;
+    //     }
+    //     ret a;
+    // }
+
+    // var x: int = 6;
+    // var y: int = 10;
+    // var z: int = 0;
+    // z = fib(x + y);
+    // print(z);
+    // ";
+
+    let code = "
+        proc a(x: int, y: int) -> int {
+            print(y);
+            ret x + 1;
+        }
+
+        var b = a(5, 7);
+        var c = 10;
+        var d = b + c;
+
+        print(b);
+
+    ";
 
     let tokens = tokenize(code);
     let source_lines = sourcelines::SourceLines::new(code);
@@ -37,7 +68,9 @@ fn main() -> Result<(), String> {
         let ast = parse(tokens.as_slice());
         println!("{:#?}", ast);
         if let Ok(ast) = ast {
-            let val = ast.eval(&mut interpreter::Environment::new());
+            type_check_program(&ast)?;
+            println!("{:#?}", ast);
+            eval_program(ast);
         } else {
             let errors = ast.unwrap_err();
             errors

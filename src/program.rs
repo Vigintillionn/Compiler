12320f9e8431db6b7a1e1781e3efd50fdb8500d9
Vigintillionn <yarne.ramakers@gmail.com>
2025-04-17 17:11:25 +0200

    De-overengineerd the program struct

--- src/program.rs
+++ src/program.rs
@@ -3,34 +3,42 @@ use std::marker::PhantomData;
 use crate::parser::ast::Block;
 
 #[derive(Debug)]
-pub enum Unchecked {}
+pub struct Unchecked;
 #[derive(Debug)]
-pub enum TypeChecked {}
+pub struct TypeChecked;
 #[derive(Debug)]
-pub enum CfChecked {}
+pub struct CfChecked;
 
 #[derive(Debug)]
-pub struct Program<Ph> {
+pub struct UncheckedProgram {
     pub stmts: Block,
-    _phase: PhantomData<Ph>,
+    _phase: Unchecked,
 }
 
-pub type UncheckedProgram = Program<Unchecked>;
-pub type TypeCheckedProgram = Program<TypeChecked>;
-pub type CfCheckedProgram = Program<CfChecked>;
+#[derive(Debug)]
+pub struct TypeCheckedProgram {
+    pub stmts: Block,
+    _phase: TypeChecked,
+}
+
+#[derive(Debug)]
+pub struct CfCheckedProgram {
+    pub stmts: Block,
+    _phase: CfChecked,
+}
 
 impl UncheckedProgram {
     pub fn new(stmts: Block) -> Self {
         Self {
             stmts,
-            _phase: PhantomData,
+            _phase: Unchecked,
         }
     }
 
     pub fn into_type_checked(self) -> TypeCheckedProgram {
         TypeCheckedProgram {
             stmts: self.stmts,
-            _phase: PhantomData,
+            _phase: TypeChecked,
         }
     }
 }
@@ -39,7 +47,7 @@ impl TypeCheckedProgram {
     pub fn into_cf_checked(self) -> CfCheckedProgram {
         CfCheckedProgram {
             stmts: self.stmts,
-            _phase: PhantomData,
+            _phase: CfChecked,
         }
     }
 }
